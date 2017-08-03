39be0725522ebbb56dcfc5f3850a4cd9cf5c97ac
matrixback <18302835641@163.com>
2017-08-04 01:21:29 +0800

    逻辑回归初步完成

--- 数值计算/grad_descend.py
+++ 数值计算/grad_descend.py
@@ -2,8 +2,6 @@
 import numpy as np
 
 def grad(x, y):
-    print x
-    print y
     """x 为二维矩阵，一列表示一个记录，注意转换。theta, y 都为行向量。"""
     m, n = x.shape                   # 样本个数
     epsilon = 0.000000000001
@@ -33,10 +31,49 @@ def grad(x, y):
     print 'Done: theta: {}'.format(theta)
     print '迭代次数: %d' % cnt
 
-if __name__ == '__main__':
+def grad_2(x, y):
+    """不按公式的写法，按 x 一行表示一个记录的写法编程。对应，theta, y 都为列向量。"""
+    m, n = x.shape                   # 样本个数
+    epsilon = 0.000000000001
+    alpha = 0.01
+    theta = np.zeros((n, 1))
+    print theta
+    diff = np.zeros((n, 1))
+    max_itor = 10000
+    cnt = 0
+    err = 0
+    last_err = 0
+    while cnt < max_itor:
+        cnt += 1
+        diff = np.dot(x, theta) - y
+        theta -= alpha * np.dot(x.transpose(), diff)
+        new_diff = np.dot(x, theta) - y
+        err = sum(new_diff * new_diff) / 2
+        if abs(err - last_err) < epsilon:
+            break
+        else:
+            last_err = err
+
+        print ' theta: {} error1: {}'.format(theta, err)
+
+    print 'Done: theta: {}'.format(theta)
+    print '迭代次数: %d' % cnt
+
+def test_1():
     x = np.array([(1, 1, 2), (1, 1, 3), (1, 1, 4), (1, 1, 5), (1, 1, 6), (1, 1, 7)])
     y = np.array([4, 6, 8, 10, 12, 14])
     # 将 x 进行转置，每一列表示一个记录，方便与数学公式对应。
     x = x.transpose()
     grad(x, y)
+
+def test_2():
+    x = np.array([(1, 1, 2), (1, 1, 3), (1, 1, 4), (1, 1, 5), (1, 1, 6), (1, 1, 7)])
+    # 注意，一维的array 转置还是一维，必须二维的才能转置。所以最好用 mat，不用 array.
+    y = np.array([[4, 6, 8, 10, 12, 14]]).transpose()
+    print y
+    grad_2(x, y)
+
+
+if __name__ == '__main__':
+    test_1()
 
---
+++ 机器学习实战/chapter5/log_regres.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+def load_set():
+    data = []
+    label = []
+    fr = open('testSet.txt', 'r')
+    for line in fr.readlines():
+        line_arr = line.strip().split()
+        data.append([1.0, float(line_arr[0]), float(line_arr[1])])
+        label.append(int(line_arr[2]))
+    return data, label
+
+def sigmoid(in_x):
+    return 1.0/(1 + np.exp(-in_x))
+
+def grad_ascent(data_list, class_label):
+    """我们需要求的是损失函数的最大值？"""
+    data_mat = np.mat(data_list)
+    label_mat = np.mat(class_label).transpose()
+    m, n = np.shape(data_mat)
+    alpha = 0.001
+    max_cycles = 1000
+    weights = np.ones((n, 1))
+    for k in range(max_cycles):
+        # data_mat*weights 矩阵乘积，为假设值 y(i)。
+        # 将乘积再进行 sigmoid 转换，曲线拟合中，直接用乘积，而在分类中，必须转换。
+        h = sigmoid(data_mat*weights)
+        # 和书上的不一致，书上的本质也是梯度下降法，不知道人家为什么将 error = label_mat -h，然后起名叫梯度上升法
+        error = (h - label_mat)   # 应该是求导的那一步的 diff
+        # 和公式的写法正好相反，但是思路一样.
+        weights -= alpha * data_mat.transpose() * error
+    return weights
+
+def plot_best_fit(weights):
+    import matplotlib.pyplot as plt
+    data, label = load_set()
+    data_arr = np.array(data)
+    n = np.shape(data_arr)[0]
+    xcord1 = []
+    ycord1 = []
+    xcord2 = []
+    ycord2 = []
+    for i in range(n):
+        if int(label[i]) == 1:
+            xcord1.append(data_arr[i, 1]); ycord1.append(data_arr[i, 2])
+        else:
+            xcord2.append(data_arr[i, 1]); ycord2.append(data_arr[i, 2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+    x = np.arange(-3.0, 3.0, 0.1)
+    y = (-weights[0] - weights[1]*x)/weights[2]
+    ax.plot(x, y)
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.show()
+
+
+if __name__ == '__main__':
+    data, label = load_set()
+    weights = grad_ascent(data, label)
+    print weights
+    plot_best_fit(weights)
+
+
