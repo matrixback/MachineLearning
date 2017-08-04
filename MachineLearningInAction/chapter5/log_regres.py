c816309189a8b8bbb7e064e5e4b829046595d0a9
matrixback <1261305275@qq.com>
2017-08-04 18:47:12 +0800

    start ch6

--- MachineLearningInAction/chapter5/log_regres.py
+++ MachineLearningInAction/chapter5/log_regres.py
@@ -84,6 +84,52 @@ def stoc_grad_ascent1(data, label, num_iter=150):
             del(data_index[rand_index])
     return weights
 
+def classify_vector(in_x, weights):
+    prob = sigmoid(sum(in_x * weights))
+    if prob > 0.5:
+        return 1.0
+    else:
+        return 0.0
+
+def colic_test():
+    fr_train = open('horseColicTraining.txt')
+    fr_test = open('horseColicTest.txt')
+    training_set = []
+    training_labels = []
+    for line in fr_train.readlines():
+        # 这儿写的有点麻烦，应该直接 float 在 cur_line 转换，然后赋值就可以呢。
+        curr_line = line.strip().split('\t')
+        line_arr = []
+        for i in range(21):
+            line_arr.append(float(curr_line[i]))
+        training_set.append(line_arr)
+        training_labels.append(float(curr_line[21]))
+    training_weights = stoc_grad_ascent1(np.array(training_set), training_labels, 500)
+
+    error_count = 0
+    num_test_vec = 0.0
+    for line in fr_test.readlines():
+        num_test_vec += 1.0
+        curr_line = line.strip().split('\t')
+        line_arr = []
+        for i in range(21):
+            line_arr.append(float(curr_line[i]))
+        if int(classify_vector(np.array(line_arr), training_weights)) != \
+            int(curr_line[21]):
+            error_count += 1
+    error_rate = (float(error_count/num_test_vec))
+    print 'the error rate of this test is: %f' % error_rate
+    return error_rate
+
+
+def multi_test():
+    num_tests = 10
+    error_sum = 0.0
+    for k in range(num_tests):
+        error_sum += colic_test()
+    print 'after %d iterations the average error rate is %f' % (num_tests,
+                                         error_sum/float(num_tests))
+
 def do_grad_ascend():
     data, label = load_set()
     weights = grad_ascent(data, label)
@@ -99,6 +145,6 @@ def do_stoc_grad_ascend():
 
 
 if __name__ == '__main__':
-    do_stoc_grad_ascend()
+    multi_test()
 
 
